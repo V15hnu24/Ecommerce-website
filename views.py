e549c7693dddc43889ca95603cf6462fb40b7b79
samriddhsingh123 <samriddh20466@iiitd.ac.in>
2022-04-21 23:32:51 +0530

    Added python code to identify views

---
+++ views.py
@@ -0,0 +1,14 @@
+import mysql.connector
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="7061" ,database = "olx_final")
+
+
+def SeeViews():
+    mycursor = mydb.cursor()
+    query="SHOW FULL TABLES IN olx_final WHERE TABLE_TYPE LIKE 'VIEW';"
+    mycursor.execute(query)
+    myresult = mycursor.fetchall()
+    print('Views are')
+    for x in myresult:
+        print(x)
+    print()
+SeeViews()
