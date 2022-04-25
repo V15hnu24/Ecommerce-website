22c9b48783f37a5f604534cb6b9332aaf45aa25d
Jarvis1501 <88939964+Manoj2020375@users.noreply.github.com>
2022-04-25 23:19:24 +0530

    Update Grants.sql

--- Query/Grants.sql
+++ Query/Grants.sql
@@ -1,9 +1,16 @@
-show grants for root@localhost;
-
-GRANT SELECT ON TABLE user_details TO root@localhost;
-GRANT  create, select on table product_table to root@localhost;
-grant update on table user_details to root@localhost;
-grant delete on table product_table to root@localhost;
-GRANT ALTER on Table description_table to root@localhost;
-
-revoke create, select on table product_table from root@localhost;
+create user Manoj identified by "Manoj@123";
+create user  samriddh identified by "1234";
+create user vishnu identified by "6789";
+create user varun identified by "Varun@123";
+create user customer_1 identified by "0123";
+create user customer_2 identified by "3456";
+create user customer_3 identified by "qwerty@123";
+create user customer_4 identified by "asdf@1234";
+create user customer_5 identified by "zxcv@1234";
+create user owner_1 identified by "owner@123";
+GRANT TRIGGER ON table user_details to Manoj;
+grant select on * to owner_1;
+grant delete on * to owner_1;
+grant update on * to owner_1;
+grant index on * to owner_1;
+grant insert on * to owner_1;
